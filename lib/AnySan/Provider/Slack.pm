5ff34f426de7845ff4f562dcce0a8e8dd22792a7
Ichinose Shogo <shogo82148@gmail.com>
2015-09-19 18:30:14 +0900

    delete debug message

--- lib/AnySan/Provider/Slack.pm
+++ lib/AnySan/Provider/Slack.pm
@@ -43,7 +43,6 @@ sub slack {
     });
     $rtm->on('message' => sub {
         my ($rtm, $message) = @_;
-        use DDP; p $message;
         my $authinfo = $self->{authinfo} or return;
         return if $message->{subtype} && $message->{subtype} eq 'bot_message';
         return if $message->{user} && $message->{user} eq $authinfo->{user_id};
